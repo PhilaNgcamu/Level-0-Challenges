88f86a6b864a60afdd2a30dfe9e80813c6b6d22d
PhilaNgcamu <philangcamu17@gmail.com>
2021-06-28 22:45:50 +0200

    New changes on 28 June

--- Task0.10.js
+++ Task0.10.js
@@ -1,12 +1,10 @@
 function findCommonLetters(str1, str2) {
-    str1 + str2
     for(let i=0;i<str1.length;i++) {
         for(let j=0; j<str2.length;j++) {
-            if(str1[i]==str2[j] && str1[i],str2[j]) {
-                console.log(str1[i], str2[j])
-            }
+            if(str1[i]==str2[j]) {
+              console.log("Common leters: " + str1[i])
+            } 
+          }    
         }
-    }
-}
-
-findCommonLetters("computers", "house")
+      }
+  findCommonLetters("comyputers", "hyouse")
--- Task0.9.js
+++ Task0.9.js
@@ -1,8 +1,10 @@
 function findVowels(s){
-    for(let i=0;i<s.length;i++) {    
-        if(s[i]=="a"|| s[i]=="e" || s[i]=="i" || s[i]=="o" || s[i] =="u") {
+    for(let i=0;i<s.length;i++) { 
+        if (s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))){  
+          if(s[i]=="a"|| s[i]=="e" || s[i]=="i" || s[i]=="o" || s[i] =="u") {
             console.log("Vowel: " + s[i].toLowerCase())
-        } 
+        }
+      }
     }  
-} 
-findVowels("Umuzi")
+  } 
+  findVowels("Uuumuzi")

cac1bc83b4ad5a68cd7ba46fc5fc4760461a9306
PhilaNgcamu <philangcamu17@gmail.com>
2021-07-22 15:25:34 +0200

    Final changes

--- Task0.10.js
+++ Task0.10.js
@@ -1,10 +1,12 @@
 function findCommonLetters(str1, str2) {
-    for(let i=0;i<str1.length;i++) {
-        for(let j=0; j<str2.length;j++) {
-            if(str1[i]==str2[j]) {
-              console.log("Common leters: " + str1[i])
-            } 
-          }    
-        }
+  var a ="";
+  for(let i=0;i<str1.length;i++) {
+    for(let j=0; j<str2.length; j++) {
+      if(str2[j]==str1[i]) {
+        a+=str1[i]
       }
-  findCommonLetters("comyputers", "hyouse")
+    }
+      }
+    return a
+    }
+console.log(findCommonLetters("comyputers", "hyouse"))
